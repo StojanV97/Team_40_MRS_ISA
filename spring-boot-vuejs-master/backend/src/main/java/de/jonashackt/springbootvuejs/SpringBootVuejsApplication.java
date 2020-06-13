e617910dc530995d514b4300ef80170b09080ff6
Stojan Velickovic <stojan.v1997@outlook.com>
2020-06-13 13:00:44 +0200

    Odradjena pretraga slobodnih soba za datum i pronalazenje prvog slobodnog datuma za sve sobe.

--- spring-boot-vuejs-master/backend/src/main/java/de/jonashackt/springbootvuejs/SpringBootVuejsApplication.java
+++ spring-boot-vuejs-master/backend/src/main/java/de/jonashackt/springbootvuejs/SpringBootVuejsApplication.java
@@ -26,7 +26,7 @@ public class SpringBootVuejsApplication {
 
 	}
 	@Bean
-	CommandLineRunner runner(ClinicRoomRepository clinicRoomRepository,ClinicDoctorRepository clinicDoctorRepository,UserRepository userRepository, RoomRepository roomRepository, RequestRepository requestRepository, ClinicRepository clinicRepository, MedicineRepository medicineRepository, DiagnoseRepository diagnoseRepository){
+	CommandLineRunner runner(AppointmentRequestRepository appointmentRequestRepository,ClinicRoomRepository clinicRoomRepository,ClinicDoctorRepository clinicDoctorRepository,UserRepository userRepository, RoomRepository roomRepository, RequestRepository requestRepository, ClinicRepository clinicRepository, MedicineRepository medicineRepository, DiagnoseRepository diagnoseRepository){
 		return args -> {
 
 			//Date
